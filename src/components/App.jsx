d5a1a50fe1511c796781e7baa31b0d6d629ee903
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-07 22:10:05 +0100

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import css from './App.module.css';
+
+class App extends React.Component {
+  state = {
+    //
+  };
+
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
