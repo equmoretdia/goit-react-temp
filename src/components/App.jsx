4b5782a495ec182fa45e2331d8f8011b8668a689
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-08 00:41:40 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,5 @@
-import PageTitle from './PageTitle/PageTitle';
-import EventBoard from './EventBoard/EventBoard';
-
-import upcomingEvents from '../upcoming-events.json';
-
 const App = () => {
-  return (
-    <div>
-      <PageTitle text="24th core worlds coalition conference" />
-      <EventBoard events={upcomingEvents} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
