9e6ef90ed2b36d2045543026a4904e86d6270b55
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-09 00:24:42 +0100

    api interface is in separate module

---
+++ src/services/pokemon-api.js
@@ -0,0 +1,8 @@
+export default function fetchPokemon(name) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(new Error(`there is no pokemon with "${name}" name`));
+  });
+}
