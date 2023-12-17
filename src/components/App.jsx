6b0d48a41c2c09961a671c212c81749ee98379cf
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-17 18:34:54 +0100

    useContext

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import AppBar from './AppBar/AppBar';
 import SignupForm from './SignupForm/SignupForm';
 import ColorPicker from './ColorPicker/ColorPicker';
 import Counter from './Counter/Counter';
@@ -9,6 +10,7 @@ import colors from 'data/colors.json';
 export default function App() {
   return (
     <>
+      {/* <AppBar /> */}
       <SignupForm />
       <ColorPicker options={colors} />
       <Counter />
