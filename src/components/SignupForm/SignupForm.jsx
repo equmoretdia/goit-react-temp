b46706441305b0b7494f0391f3036f8bee6f3539
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-17 13:50:15 +0100

    localStorage and custom hook

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,13 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from './SignupForm.module.css';
+
+const useLocalStorage = (
+  key,
+  defaultValue,
+  serialize = JSON.stringify,
+  deserialize = JSON.parse
+) => {
+  const [state, setState] = useState(() => {
+    return deserialize(window.localStorage.getItem(key)) ?? defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
+  return [state, setState];
+};
 
 export default function SignupForm() {
   //   const [inputState] = useState('');
 
   //   const [state, setState] = useState({ email: '', password: '' });
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  // // The nullish coalescing operator checks if the result of the parsing
+  // // (JSON.parse(...)) is null or undefined. If it is, it uses the default
+  // // value on the right side of the operator, which is an empty string ''.
+  // //
+  // // console.log(0 || 1); //1
+  // // console.log(0 ?? 1); //0
+  // // console.log(false || 1); //1
+  // // console.log(false ?? 1); //false
+  // // console.log(undefined || 1); //1
+  // // console.log(undefined ?? 1); //1
+  // // console.log(null || 1); //1
+  // // console.log(null ?? 1); //1
+  // //
+
+  // if we code like this the request to localStorage will be
+  // implemented for every render, this is how JS interpretor
+  // works...
+  // const [email, setEmail] = useState(
+  //   JSON.parse(window.localStorage.getItem('email')) ?? ''
+  // );
+  // const [password, setPassword] = useState(
+  //   JSON.parse(window.localStorage.getItem('password')) ?? ''
+  // );
+  //...thus lazy state initialization need to be used (useState
+  // will run this code only once during the initial render of the
+  // component):
+  // const [email, setEmail] = useState(() => {
+  //   return JSON.parse(window.localStorage.getItem('email')) ?? '';
+  // });
+  // const [password, setPassword] = useState(() => {
+  //   return JSON.parse(window.localStorage.getItem('password')) ?? '';
+  // });
+
+  //now we use useLocalStorage custom hook:
+  const [email, setEmail] = useLocalStorage('email', '');
+  const [password, setPassword] = useLocalStorage('password', '');
 
   //   console.log(setEmail);
 
@@ -45,6 +97,14 @@ export default function SignupForm() {
         return;
     }
   };
+
+  // useEffect(() => {
+  //   window.localStorage.setItem('email', JSON.stringify(email));
+  // }, [email]);
+
+  // useEffect(() => {
+  //   window.localStorage.setItem('password', JSON.stringify(password));
+  // }, [password]);
 
   return (
     <form className={css.form} autoComplete="off">
