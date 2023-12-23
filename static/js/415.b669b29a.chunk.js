32ee1ac274fd407407e061cacf4496c8ce92442b
equmoretdia <serhiy.belinskyy@gmail.com>
2023-12-23 21:54:31 +0000

    Deploying to gh-pages from @ equmoretdia/goit-react-temp@114d78cb50997c19239dd408804a6590a9a72df1 🚀

---
+++ static/js/415.b669b29a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Home page"})}}}]);
+//# sourceMappingURL=415.b669b29a.chunk.js.map
