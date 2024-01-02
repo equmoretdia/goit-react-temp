5e4c8435a27e579660ba61c2f12b0fa77c1cda00
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-02 23:07:14 +0100

    store is refactored with createSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+// import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const increment = createAction('myCounter/increment');
-export const decrement = createAction('myCounter/decrement');
-
-console.log([increment.toString()]);
-
-const myReducer = createReducer(10, builder => {
-  builder.addCase(increment, (state, action) => state + action.payload);
-  builder.addCase(decrement, (state, action) => state - action.payload);
+const myValueSlice = createSlice({
+  name: 'myValue',
+  initialState: 10,
+  reducers: {
+    increment(state, action) {
+      return state + action.payload;
+    },
+    decrement(state, action) {
+      return state - action.payload;
+    },
+  },
 });
+
+console.log(myValueSlice);
+
+export const { increment, decrement } = myValueSlice.actions;
+
+// export const increment = createAction('myCounter/increment');
+// export const decrement = createAction('myCounter/decrement');
+
+// console.log([increment.toString()]);
+
+// const myReducer = createReducer(10, builder => {
+//   builder.addCase(increment, (state, action) => state + action.payload);
+//   builder.addCase(decrement, (state, action) => state - action.payload);
+// });
 
 export const store = configureStore({
   reducer: {
-    myValue: myReducer,
+    myValue: myValueSlice.reducer,
   },
 });
