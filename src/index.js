afe791a0d9499b5eb61133b335079e9d23d978e7
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-02 19:19:20 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
 const root = document.getElementById('root');
 ReactDom.createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-temp">
-      {/* <BrowserRouter basename="/goit-react-temp"> - for git pages*/}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
