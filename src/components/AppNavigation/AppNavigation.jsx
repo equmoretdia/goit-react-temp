47b558128f294eeeecf96da1b7cc5b0f9896890c
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-03 22:04:56 +0100

    AppNavigation content update: logical checks for navlinks rendering are added

--- src/components/AppNavigation/AppNavigation.jsx
+++ src/components/AppNavigation/AppNavigation.jsx
@@ -1,14 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export const AppNavigation = () => {
+  const isLoggedIn = useSelector(state => state.userAuth.isLoggedIn);
   return (
     <ul>
-      <li>
-        <NavLink to="/login">Log in</NavLink>
-      </li>
-      <li>
-        <NavLink to="/counter">Counter</NavLink>
-      </li>
+      {!isLoggedIn && (
+        <li>
+          <NavLink to="/login">Log in</NavLink>
+        </li>
+      )}
+      {isLoggedIn && (
+        <>
+          <li>
+            <NavLink to="/counter">Counter</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
