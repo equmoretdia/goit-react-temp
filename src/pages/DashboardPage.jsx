ad4b953effb6fa3780e7b1a9ba518c3194b32538
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-03 21:53:23 +0100

    refactoring: hook uselogOutRedirect is splitted from the DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,15 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useLogOutRedirect } from 'hooks/useLogOutRedirect';
 
 export const DashboardPage = () => {
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector(state => state.userAuth.isLoggedIn);
+  useLogOutRedirect();
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/login', { replace: true });
-    }
-  }, [isLoggedIn, navigate]);
   return <div>Dashboard Page</div>;
 };
