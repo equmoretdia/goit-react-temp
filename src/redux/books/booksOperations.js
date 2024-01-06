ff44aef275f5bd42c176e37ff2a9c24d6b7ca04d
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-06 18:11:53 +0100

    booksOperations is added

--- src/redux/books/booksOperations.js
+++ src/redux/books/booksOperations.js
@@ -1,5 +1,16 @@
 // import { createAsyncThunk } from '@reduxjs/toolkit';
-// import * as bookShelfAPI from 'services/bookshelf-api';
+import * as bookShelfAPI from 'services/bookshelf-api';
+import * as booksActions from './booksActions';
+
+export const fetchBooks = () => async dispatch => {
+  dispatch(booksActions.fetchBooksRequest());
+  try {
+    const books = await bookShelfAPI.fetchBooks();
+    dispatch(booksActions.fetchBooksSuccess(books));
+  } catch (error) {
+    dispatch(booksActions.fetchBooksError(error));
+  }
+};
 
 // export const fetchBooks = createAsyncThunk(
 //   'books/fetchBooks',
