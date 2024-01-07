768586e9713cb5011f0baa657b87bca13fd6f2a7
equmoretdia <serhiy.belinskyy@gmail.com>
2024-01-07 21:30:23 +0000

    Deploying to gh-pages from @ equmoretdia/goit-react-temp@e8d6ab57745fbc27ff0de65fadb0f2bf8afba02b 🚀

--- static/js/books-view.13939ba1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[456],{812:function(e,t,n){n.d(t,{Z:function(){return c}});var r="PageHeading_title__1wPIF",a=n(184);function c(e){var t=e.text;return(0,a.jsx)("h1",{className:r,children:t})}},607:function(e,t,n){n.r(t);var r=n(812),a=n(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(r.Z,{text:"Books"})})}}}]);
-//# sourceMappingURL=books-view.13939ba1.chunk.js.map
