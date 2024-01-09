4be1ccf2a950daae8e22347827720d56049e6383
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-09 21:19:29 +0100

    booksReducer refactoring: createSlice instead of reducers with operations

--- src/redux/books/booksReducer.js
+++ src/redux/books/booksReducer.js
@@ -1,6 +1,6 @@
-import { createReducer, combineReducers } from '@reduxjs/toolkit';
+// import { createReducer, combineReducers } from '@reduxjs/toolkit';
 // import * as booksActions from './booksActions';
-// import { createReducer, combineReducers, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { fetchBooks } from './booksOperations';
 
 //1st step: reducers with actions
@@ -25,37 +25,50 @@ import { fetchBooks } from './booksOperations';
 // });
 
 //2nd step: reducers with operations
-const entities = createReducer([], builder => {
-  builder.addCase(fetchBooks.fulfilled, (_, action) => action.payload);
+// const entities = createReducer([], builder => {
+//   builder.addCase(fetchBooks.fulfilled, (_, action) => action.payload);
+// });
+
+// const isLoading = createReducer(false, builder => {
+//   builder
+//     .addCase(fetchBooks.pending, () => true)
+//     .addCase(fetchBooks.fulfilled, () => false)
+//     .addCase(fetchBooks.rejected, () => false);
+// });
+
+// const error = createReducer(null, builder => {
+//   builder
+//     .addCase(fetchBooks.rejected, (_, action) => action.payload)
+//     .addCase(fetchBooks.pending, () => null);
+// });
+
+// export default combineReducers({
+//   entities,
+//   isLoading,
+//   error,
+// });
+
+//3rd step: createSlice
+// 🔥 USES IMMER FOR STATE COPY MUTATION (it works with copy of state and then add
+// it to the state w / o its mutation)
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: { entities: [], isLoading: false, error: null },
+  extraReducers: builder => {
+    // [fetchBooks.fulfilled]: (state, action) => {
+    //   return {
+    //     ...state,
+    //     // entities: [...state.entities, ...action.payload] // if we need to add
+    //     entities: action.payload, //but we need to replace
+    //   };
+    // },
+    builder
+      .addCase(
+        fetchBooks.fulfilled,
+        (state, { payload }) => (state.entities = payload)
+      )
+      .addCase(fetchBooks.pending, state => (state.isLoading = true));
+  },
 });
 
-const isLoading = createReducer(false, builder => {
-  builder
-    .addCase(fetchBooks.pending, () => true)
-    .addCase(fetchBooks.fulfilled, () => false)
-    .addCase(fetchBooks.rejected, () => false);
-});
-
-const error = createReducer(null, builder => {
-  builder
-    .addCase(fetchBooks.rejected, (_, action) => action.payload)
-    .addCase(fetchBooks.pending, () => null);
-});
-
-export default combineReducers({
-  entities,
-  isLoading,
-  error,
-});
-
-// // 🔥 ИСПОЛЬЗУЕТ IMMER ДЛЯ МУТАЦИИ КОПИИ СОСТОЯНИЯ
-// // const booksSlice = createSlice({
-// //   name: 'books',
-// //   initialState: { entities: [], isLoading: false, error: null },
-// //   extraReducers: {
-// //     [fetchBooks.fulfilled]: (state, { payload }) => (state.entities = payload),
-// //     [fetchBooks.pending]: state => (state.isLoading = true),
-// //   },
-// // });
-
-// // export default booksSlice.reducer;
+export default booksSlice.reducer;
